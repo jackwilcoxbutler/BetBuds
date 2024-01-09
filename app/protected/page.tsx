413a4d3182be12e4fd1c702b24613e952bb8b8cb
JW <jackwilcox1210@gmail.com>
2024-01-09 00:08:17 -0600

    Header Refactor

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,28 +1,11 @@
-import SignOut from "@/components/signout_button";
 import { ListLeagues } from "@/components/leagues/ListLeagues";
 import OpenLeagueFormButton from "@/components/leagues/CreateLeagueButton";
-import mySvg from '@components/radiant-gradient.svg';
-import { url } from "inspector";
-import InboxModal from "@/components/Invitations/InboxModal";
+import DefaultHeader from "@/components/header";
 
 export default function Home() {
   return (
-  <div className="h-screen w-screen bg-min">
-    <header className="bg-t-dark-blue sticky top-0">
-        {/*replace with homebutton component*/}
-        <div className="flex items-center justify-between p-4 bg-t-dark-blue mx-20">
-          <a
-            href="/protected"
-            className="text-2xl font-bold text-t-orange"
-          >
-            BetBuds
-          </a>
-          <div className="flex space-x-8 items-center">
-            <InboxModal/>
-            <SignOut />
-          </div>
-        </div>
-      </header>
+  <div className="h-screen w-screen bg-t-white">
+    <DefaultHeader/>
         <div className="w-full flex justify-center">
           <div className="flex flex-row w-5/6 h-screen justify-end space-x-4">
               <div className="text-t-dark-blue">
---
+++ components/header.tsx
@@ -0,0 +1,24 @@
+import InboxModal from "./Invitations/InboxModal"
+import SignOut from "./signout_button"
+
+export const DefaultHeader = () => {
+    return(
+        <header className="bg-t-dark-blue sticky top-0">
+        {/*replace with homebutton component*/}
+        <div className="flex items-center justify-between p-4 bg-t-dark-blue mx-20">
+          <a
+            href="/protected"
+            className="text-2xl font-bold text-t-orange"
+          >
+            BetBuds
+          </a>
+          <div className="flex space-x-8 items-center">
+            <InboxModal/>
+            <SignOut />
+          </div>
+        </div>
+      </header>
+    )
+}
+
+export default DefaultHeader;
