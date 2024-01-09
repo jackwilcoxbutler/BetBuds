012f36d64fb50f2dba82ab10a746a1727f40fb56
JW <jackwilcox1210@gmail.com>
2024-01-09 13:38:08 -0600

    search done + style

--- components/UserSearch.tsx
+++ components/UserSearch.tsx
@@ -69,11 +69,11 @@ export default function SearchUser() {
       {searchResults && (
       <ScrollArea.Root className="w-[200px] h-[100px] rounded overflow-hidden shadow-[0_2px_10px] shadow-blackA4 bg-white">
         <ScrollArea.Viewport className="w-full h-full rounded">
-          <div className="py-[15px] px-5">
+          <div className="py-[5px] px-5">
             {searchResults.map((user) => (
               <div key={user.id}>
                 <button
-                className="text-mauve12 text-[13px] leading-[18px] mt-2.5 pt-2.5 border-t border-t-mauve6"
+                className="text-mauve12 text-[13px] leading-[18px] mt-1 pt-1 border-t border-t-mauve6"
                 onClick={() => {context?.setUsername(user.username); setSearchResults(null);debouncedSearch.cancel();}}
               >
                 {user.username}
@@ -86,7 +86,7 @@ export default function SearchUser() {
           className="flex select-none touch-none p-0.5 bg-blackA3 transition-colors duration-[160ms] ease-out hover:bg-blackA5 data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col data-[orientation=horizontal]:h-2.5"
           orientation="vertical"
         >
-          <ScrollArea.Thumb className="flex-1 bg-mauve10 rounded-[10px] relative before:content-[''] before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:w-full before:h-full before:min-w-[44px] before:min-h-[44px]" />
+          <ScrollArea.Thumb className="flex-1 rounded-[10px] relative before:content-[''] before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:w-full before:h-full before:min-w-[44px] before:min-h-[44px]" />
         </ScrollArea.Scrollbar>
         <ScrollArea.Scrollbar
           className="flex select-none touch-none p-0.5 bg-blackA3 transition-colors duration-[160ms] ease-out hover:bg-blackA5 data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col data-[orientation=horizontal]:h-2.5"
