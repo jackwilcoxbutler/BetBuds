457abf34163cc55db102a16a73c946d73276abde
JW <jackwilcox1210@gmail.com>
2024-01-18 10:56:54 -0500

    fix margins

--- app/protected/home/layout.tsx
+++ app/protected/home/layout.tsx
@@ -14,12 +14,12 @@ export default async function ProtectedLayout({
         <div className="h-screen w-screen bg-t-white">
           <DefaultHeader />
           <div className="w-full flex justify-center">
-            <div className="flex flex-row w-full justify-end space-x-4">
+            <div className="flex flex-row w-full justify-end space-x-4 mx-32">
               <div className="flex flex-col w-full">
                 <SportTab />
                 {children}
               </div>
-              <div className="flex flex-col w-1/3 h-screen justify-start px-4 border-l-4 border-dotted">
+              <div className="flex flex-col w-1/3 h-screen justify-start pl-4 border-l-4 border-dotted">
                 <ListLeagues />
                 <OpenLeagueFormButton />
               </div>
