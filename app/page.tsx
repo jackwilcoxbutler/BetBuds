9f34eda4dfd868d499a11bf7809b2d76aff77ce9
JW <jackwilcox1210@gmail.com>
2024-04-19 20:44:09 -0400

    landing page for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,18 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex h-screen bg-endless-constelation">
-      <div className="w-screen h-screen ">
+      <div className="w-screen h-screen">
         <DefaultHeader />
-        <div className="h-screen flex flex-col justify-evenly text-t-dark-blue text-center mx-32 ">
+        <div className="h-screen flex flex-col justify-evenly text-t-dark-blue text-center mx-4 sm:mx-32 ">
           <div>
             <div className="flex w-full justify-center">
             <TrophyLogo height={64} weight={64}/>
             </div>
-            <h1 className="text-5xl font-bold mb-6 bg-t-white ">Join the Ultimate Sports Betting Experience</h1>
-            <p className="text-xl mb-8">Compete with friends. Make daily picks. Win big.</p>
-            <Link href={"/register"} className="bg-t-orange hover:bg-t-orange-200 text-t-white font-bold py-2 px-4 rounded text-lg">Sign Up Now</Link>
+            <h1 className="text-2xl sm:text-5xl font-bold mb-6 bg-t-white ">Join the Ultimate Sports Betting Experience</h1>
+            <p className="text-lg mb-8">Compete with friends. Make daily picks. Win big.</p>
+            <Link href={"/register"} className="bg-t-orange hover:bg-t-orange-200 text-t-white font-bold py-2 px-4 rounded text-2xl">Sign Up Now</Link>
           </div>
-          <div className="grid md:grid-cols-3 gap-8 text-center">
+          <div className="grid md:grid-cols-3 gap-4 text-center">
             <div className=" rounded-lg border p-4 bg-t-dark-blue">
               <h2 className="font-bold text-t-white text-xl mb-4">Daily Picks</h2>
               <p className="text-t-white">Choose your bets daily and challenge your friends to see who comes out on top.</p>
--- components/noUserHeader.tsx
+++ components/noUserHeader.tsx
@@ -7,14 +7,14 @@ export const DefaultHeader = () => {
   return (
     <header className="bg-t-dark-blue sticky top-0">
       {/*replace with homebutton component*/}
-      <div className="flex items-center justify-between p-4 bg-t-dark-blue mx-32">
+      <div className="flex items-center justify-between p-2 sm:p-4 bg-t-dark-blue mx-4 sm:mx-32">
         <a
           href="/protected/home/americanfootball_nfl"
-          className="text-2xl font-bold text-t-grey"
+          className="text-lg sm:text-2xl font-bold text-t-orange"
         >
          BetBuds
         </a>
-        <div className="flex space-x-8 items-center">
+        <div className="flex space-x-2 sm:space-x-8 items-center">
           <AuthButton />
         </div>
       </div>
