47c4369348ea99d6aa80ca635facce2146c19d64
JW <jackwilcox1210@gmail.com>
2024-04-19 20:51:41 -0400

    overscroll login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import Form from "@/components/form";
 
 export default function Login() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-t-white mx-4">
+    <div className="flex h-screen w-screen items-center justify-center bg-t-white mx-4 overscroll-contain">
       <div className="z-10 sm:w-full overflow-hidden rounded-2xl border border-t-dark-blue shadow-xl bg-t-dark-blue">
         <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-16">
           {/* Logo */}
